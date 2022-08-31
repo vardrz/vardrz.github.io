6fcc40929db295c2831e61e16d3d6a6e3787a0c7
vardrz <fatkhurrozakf@gmail.com>
2022-08-31 10:56:00 +0700

    update view

---
+++ index.php
@@ -0,0 +1,393 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <title>[Resume] Farid Fatkhurrozak</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="resume" name="keywords">
+  <meta content="Resume Farid Fatkhurrozak" name="description">
+
+  <!-- Favicons -->
+  <link href="img/favicon.png" rel="icon">
+  <link href="img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Bootstrap CSS File -->
+  <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Libraries CSS Files -->
+  <link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="lib/animate/animate.min.css" rel="stylesheet">
+  <link href="lib/ionicons/css/ionicons.min.css" rel="stylesheet">
+  <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+
+  <!-- Main Stylesheet File -->
+  <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body id="page-top">
+
+  <!--/ Nav Star /-->
+  <nav class="navbar navbar-b navbar-trans navbar-expand-md fixed-top" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand js-scroll" href="#page-top">VardRZ</a>
+      <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarDefault" aria-controls="navbarDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+      <div class="navbar-collapse collapse justify-content-end" id="navbarDefault">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link js-scroll active" href="#home">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll" href="#about">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll" href="#work">Project</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll" href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!--/ Nav End /-->
+
+  <!--/ Intro Skew Star /-->
+  <div id="home" class="intro route bg-image" style="background-image: url(img/bg.jpg)">
+    <div class="overlay-itro"></div>
+    <div class="intro-content display-table">
+      <div class="table-cell">
+        <div class="container">
+          <h1 class="intro-title mb-4">Farid Fatkhurrozak</h1>
+          <p class="intro-subtitle"><span class="text-slider-items">Backend Web Developer,Junior Web Developer</span><strong class="text-slider"></strong></p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--/ Intro Skew End /-->
+
+  <div class="about-mf sect-pt4 route paralax-mf bg-image" style="background-image: url(img/bg.jpg)">
+    <div class="overlay-mf"></div>
+    <div class="container mt-5" id="about">
+      <div class="row">
+        <div class="col-sm-12">
+          <div class="box-shadow-full">
+            <div class="row">
+              <div class="col-md-6">
+                <div class="row">
+                  <div class="col-sm-3 d-flex justify-content-center">
+                    <div class="about-img d-flex justify-content-center">
+                      <img src="img/me.jpg" class="img-fluid rounded-circle b-shadow-a" />
+                    </div>
+                  </div>
+                  <div class="col-sm-9">
+                    <table width="100%" style="font-size: 17px;">
+                      <tr>
+                        <td>
+                          <ion-icon name="person"></ion-icon>
+                        </td>
+                        <td>Farid Fatkhurrozak</td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <ion-icon name="code-slash"></ion-icon>
+                        </td>
+                        <td>Junior Web Developer</td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <ion-icon name="mail"></ion-icon>
+                        </td>
+                        <td><a href="mailto:fatkhurrozakf@gmail.com">fatkhurrozakf@gmail.com</a></td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <ion-icon name="logo-github"></ion-icon>
+                        </td>
+                        <td><a href="https://github.com/vardrz">Github.com/vardrz</a></td>
+                      </tr>
+                    </table>
+                  </div>
+                </div>
+                <div class="skill-mf mt-3">
+                  <p class="title-s h5 mb-3">Programming Skills</p>
+                  <div class="row">
+                    <div class="col-sm-4">
+                      <span style="font-size: 15px;">Languages</span>
+                      <div class="d-flex align-items-center mt-1">
+                        <img src="https://stack-icons.showwcase.com/PHP.jpg" width="25px" style="border-radius: 15px;"><span class="ml-2" style="font-size: 15px;">PHP</span>
+                      </div>
+                      <div class="d-flex align-items-center mt-2">
+                        <img src="https://stack-icons.showwcase.com/JavaScript.png" width="25px" style="border-radius: 10px;"><span class="ml-2" style="font-size: 15px;">JavaScript</span>
+                      </div>
+                      <div class="d-flex align-items-center mt-2 mb-3">
+                        <img src="https://stack-icons.showwcase.com/Dart.png" width="25px" style="border-radius: 15px;"><span class="ml-2" style="font-size: 15px;">Dart</span>
+                      </div>
+                    </div>
+
+                    <div class="col-sm-4">
+                      <span style="font-size: 15px;">Web Dev</span>
+                      <div class="d-flex align-items-center mt-1">
+                        <img src="https://stack-icons.showwcase.com/CodeIgniter.png" width="25px" style="border-radius: 15px;"><span class="ml-2" style="font-size: 15px;">CodeIgniter</span>
+                      </div>
+                      <div class="d-flex align-items-center mt-2">
+                        <img src="https://stack-icons.showwcase.com/Laravel.jpg" width="25px" style="border-radius: 15px;"><span class="ml-2" style="font-size: 15px;">Laravel</span>
+                      </div>
+                      <div class="d-flex align-items-center mt-2 mb-3">
+                        <img src="https://stack-icons.showwcase.com/expressjs.png" width="25px" style="border-radius: 15px;"><span class="ml-2" style="font-size: 15px;">Express.js</span>
+                      </div>
+                    </div>
+
+
+                    <div class="col-sm-4">
+                      <span style="font-size: 15px;">Databases</span>
+                      <div class="d-flex align-items-center mt-1">
+                        <img src="https://stack-icons.showwcase.com/MySQL.png" width="25px" style="border-radius: 15px;"><span class="ml-2">MySQL</span>
+                      </div>
+                      <div class="d-flex align-items-center mt-2 mb-3">
+                        <img src="https://stack-icons.showwcase.com/MongoDB.png" width="25px" style="border-radius: 15px;"><span class="ml-2">MongoDB</span>
+                      </div>
+                    </div>
+
+                    <div class="col-sm-4">
+                      <span style="font-size: 15px;">CSS Framework</span>
+                      <div class="d-flex align-items-center mt-1 mb-3">
+                        <img src="https://stack-icons.showwcase.com/Bootstrap.png" width="25px" style="border-radius: 15px;"><span class="ml-2">Bootstrap</span>
+                      </div>
+                    </div>
+
+                    <div class="col-sm-8">
+                      <span style="font-size: 15px;">Android Dev</span>
+                      <div class="d-flex align-items-center mt-1 mb-3">
+                        <img src="https://stack-icons.showwcase.com/Flutter.png" width="25px" style="border-radius: 15px;"><span class="ml-2">Flutter (Dart)</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="about-me pt-4 pt-md-0">
+                  <div class="title-box-2 mb-4">
+                    <h5 class="title-left">
+                      About me
+                    </h5>
+                  </div>
+                  <p class="lead">
+                    Saya adalah seorang web developer yang berfokus di bagian backend. saat ini saya berstatus sebagai mahasiswa aktif semester 2 di STMIK Widya Pratama Pekalongan, mengambil program studi S1 Teknik Informatika.
+                  </p>
+                  <p class="lead">
+                    Saya menguasai bahasa pemprograman PHP dengan framework CodeIgniter dan Laravel. terbiasa menggunakan Git Version Control System, selalu update teknologi web terbaru dan masih mempelajari berbagai bahasa pemprograman lain seperti Python Flask dan Node JS.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!--/ Section Portfolio Star /-->
+  <section id="work" class="portfolio-mf sect-pt4 route">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12">
+          <div class="title-box text-center">
+            <h3 class="title-a">
+              Project
+            </h3>
+            <div class="line-mf"></div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-4">
+          <div class="work-box">
+            <a style="cursor: pointer;" data-toggle="modal" data-target="#sipema">
+              <div class="work-img">
+                <img src="img/work-1.jpg" alt="SiPeMa" class="img-fluid">
+              </div>
+              <div class="work-content">
+                <div class="row">
+                  <div class="col">
+                    <h2 class="w-title"><a style="cursor: pointer;" data-toggle="modal" data-target="#sipema">SiPeMa - Web Pengaduan Masyarakat</a></h2>
+                    <div class="w-more">
+                      <span class="w-ctegory">CodeIgniter</span> / <span class="w-date">9 April 2021</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="paralax-mf footer-paralax bg-image sect-mt4 route" style="background-image: url(img/bg.jpg)">
+    <div class="overlay-mf"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12">
+          <div class="contact-mf">
+            <div id="contact" class="box-shadow-full">
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="title-box-2">
+                    <h5 class="title-left">
+                      Hubungi Saya
+                    </h5>
+                  </div>
+                  <div>
+                    <form action="/mail.php" method="post" role="form" class="contactForm">
+                      <?php
+                      if (isset($_SESSION['pesan'])) {
+                        echo "<div class='alert alert-primary alert-dismissible fade show d-flex' role='alert'>" . $_SESSION['pesan'] . "<a class='text-primary' style='margin-inline-start:auto; margin-inline-end:-40px; cursor:pointer;' data-bs-dismiss='alert' aria-label='Close'>x</a></div>";
+                        unset($_SESSION['pesan']);
+                      }
+                      ?>
+                      <div class="row">
+                        <div class="col-md-12">
+                          <div class="form-group">
+                            <input type="email" name="email" class="form-control" id="email" placeholder="Email anda" required>
+                          </div>
+                        </div>
+                        <div class="col-md-12 mb-3">
+                          <div class="form-group">
+                            <textarea class="form-control" name="pesan" rows="6" placeholder="Pesan" required></textarea>
+                          </div>
+                        </div>
+                        <div class="col-md-12">
+                          <button type="submit" class="button button-a button-big button-rouded">Kirim Pesan</button>
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="title-box-2 pt-4 pt-md-0">
+                    <h5 class="title-left">
+                      Detail Informasi
+                    </h5>
+                  </div>
+                  <div class="more-info">
+                    <p class="lead">
+                      Anda bisa menghubungi saya lewat kontak dibawah ini atau via sosial media.
+                    </p>
+                    <ul class="list-ico">
+                      <li><a href="https://wa.me/6289525196861"><span class="ion-social-whatsapp"></span> +62 895-2519-6861</a></li>
+                      <li><a href="mailto:fatkhurrozakf@gmail.com"><span class="ion-email"></span> fatkhurrozakf@gmail.com</a></li>
+                    </ul>
+                  </div>
+                  <div class="socials">
+                    <ul>
+                      <li><a href="https://facebook.com/vardrz"><span class="ico-circle"><i class="ion-social-facebook"></i></span></a></li>
+                      <li><a href="https://instagram.com/vard.rz"><span class="ico-circle"><i class="ion-social-instagram"></i></span></a></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12">
+            <div class="copyright-box">
+              <!-- <p class="copyright">&copy; Copyright <strong>DevFolio</strong>. All Rights Reserved</p> -->
+              <div class="credits">Vard<b>RZ</b></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </section>
+  <!--/ Section Contact-footer End /-->
+
+  <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+  <div id="preloader"></div>
+
+  <!-- Modal -->
+  <!-- SiPeMa -->
+  <div class="modal fade" id="sipema" tabindex="-1" role="dialog" aria-labelledby="sipema" aria-hidden="true">
+    <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLongTitle">SiPeMa - Sistem Pengaduan Masyarakat</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <b>SiPeMa</b> - website pengaduan dan aspirasi masyarakat, dibuat menggunakan framework CodeIgniter 3 dan di bagian pengelolaan database-nya menggunakan MySql.
+          <br>Terdapat 2 halaman dashboard, User dan Administrator, di bagian Administrator menggunakan 2 Hak Akses yaitu Admin dan Petugas.
+          <br>Project ini dibuat sebagai syarat untuk Ujian Praktik SMK Muhammadiyah Pencongan.
+          <p class="mt-2">
+            <b>User Modul :</b>
+            <br>1. Login & Register
+            <br>2. Membuat Laporan Pengaduan
+            <br>3. Kirim hanya 1 Laporan/hari
+          </p>
+          <p class="mt-2">
+            <b>Admin Modul :</b>
+            <br>1. Data Laporan Berdasarkan Status
+            <br>2. Verifikasi dan Tolak Laporan
+            <br>3. Memberi Tanggapan
+            <br>4. Export Laporan to PDF
+            <br>5. Add User / Petugas
+          </p>
+          <p class="mt-2">
+            <b>Petugas Modul :</b>
+            <br>1. Data Laporan Berdasarkan Status
+            <br>2. Verifikasi dan Tolak Laporan
+            <br>3. Memberi Tanggapan
+            <br>4. Export Laporan to PDF
+          </p>
+          <p class="mt-2">
+            <b>Demo :</b> <a href="http://sipema.vard.my.id" target="_blank" class="text-primary">Lihat</a>
+          </p>
+          <img src="img/work-1.jpg" alt="SiPeMa" class="img-fluid mt-5">
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <a href="http://sipema.vard.my.id" target="_blank" class="btn btn-primary">Demo</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- JavaScript Libraries -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+
+  <!-- Ion Icon -->
+  <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+  <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+
+  <script src="lib/jquery/jquery.min.js"></script>
+  <script src="lib/jquery/jquery-migrate.min.js"></script>
+  <script src="lib/popper/popper.min.js"></script>
+  <script src="lib/bootstrap/js/bootstrap.min.js"></script>
+  <script src="lib/easing/easing.min.js"></script>
+  <script src="lib/counterup/jquery.waypoints.min.js"></script>
+  <script src="lib/counterup/jquery.counterup.js"></script>
+  <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+  <script src="lib/lightbox/js/lightbox.min.js"></script>
+  <script src="lib/typed/typed.min.js"></script>
+  <!-- Contact Form JavaScript File -->
+  <script src="contactform/contactform.js"></script>
+
+  <!-- Template Main Javascript File -->
+  <script src="js/main.js"></script>
+
+</body>
+
+</html>
