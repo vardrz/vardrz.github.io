7810b28ae6e61186bf931d98bf050f7eb546dc73
vardrz <fatkhurrozakf@gmail.com>
2024-04-02 07:26:27 +0700

    delete all to remake

--- conn.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$conn = mysqli_connect("localhost", "root", "faridkaka8", "vard");
-$data = mysqli_query($conn, "SELECT * FROM data")->fetch_assoc();
-$projek = mysqli_query($conn, "SELECT * FROM projek");
